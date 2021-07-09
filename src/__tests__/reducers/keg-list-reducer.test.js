72221767feac368b870cd676362000f49e5219ca
Ryan Walker <ryandanielwalker@gmail.com>
2021-07-09 09:34:03 -0700

    Pass ADD_KEG keg-reducer-test, create ADD_KEG constant

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,40 @@
 import kegListReducer from "../../reducers/keg-list-reducer";
+import * as c from './../../actions/ActionTypes';
 
 describe('kegListReducer', () => {
+  let action;
+  const kegData = {
+    name: 'Rainier',
+    brand: 'Pabst',
+    price: '3',
+    abv: '4.2',
+    pintsRemaining: '124',
+    id: 1
+  }
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new keg data to masterKegList', () => {
+    const { name, brand, price, abv, pintsRemaining, id } = kegData;
+    action = {
+      type: c.ADD_KEG,
+      name: name,
+      brand: brand,
+      price: price,
+      abv: abv,
+      pintsRemaining: pintsRemaining,
+      id: id
+    };
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        abv: abv,
+        pintsRemaining: pintsRemaining,
+        id: id
+      }
+    });
   });
 })
--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,19 @@
+import * as c from './../actions/ActionTypes';
+
 export default (state = {}, action) => {
+  const { name, brand, price, abv, pintsRemaining, id } = action;
+  switch (action.type) {
+    case c.ADD_KEG:
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          price: price,
+          abv: abv,
+          pintsRemaining: pintsRemaining,
+          id: id
+        }
+      })
+  }
   return state
 }

93d335d76b5ef75b794c7de0c3515228568dac60
Ryan Walker <ryandanielwalker@gmail.com>
2021-07-09 11:44:21 -0700

    Pass initial state rootReducer tests for kegListReducer and formVisibleReducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -13,4 +13,12 @@ describe('rootReducer', () => {
       formVisibleOnPage: false
     });
   });
+
+  test('Check that the initial state of kegListReducer matches rootReducer', () => {
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, { type: null }));
+  });
+
+  test('Check that the initial state of formVisibleReducer matches rootReducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  })
 })
